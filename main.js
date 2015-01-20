d4e0bc78c1f54136ca82dc6dd85f2ef9ed8a5d14
Jorge Mario Romero Arroyo <pana8302@gmail.com>
2015-01-20 18:42:59 -0500

    firebase

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 /***********
  * UTILJS
  **********/
-var utilJS = (function() {
+var UtilJS = (function() {
   return {
     valide: function(_o) {
       return (typeof(_o) !== 'undefined')
@@ -17,8 +17,8 @@ var coreJS = (function(util) {
   var _moduleList = {};
   return {
     registerMod: function(_mod) {
-      console.log(_mod.info.id);
-      //_moduleList[_mod.info.id] = _mod;
+      console.debug('este es un ' + _mod.getControl());
+      _moduleList[_mod.getControl()] = _mod;
 
     },
     init: function() {
@@ -29,26 +29,35 @@ var coreJS = (function(util) {
       if (Object.keys(_moduleList).length)
         for (_index in _moduleList) {
           _mod = _moduleList[_index];
-          if (utilJS.valide(_mod.init))
+          if (UtilJS.valide(_mod.init))
             console.debug(_mod.info.id + ' module haven\'t \'init()\' method.');
-          else _mod.show();
+          else _mod.init();
         }
       else console.debug('There aren\'t modules registered!');
     }
   }
-}(utilJS));
+}(UtilJS));
 coreJS.init();
 /***********
  * MODULES
  **********/
+function Module = {
+  id: 'module',
+  control: 'homepage',
+  init: function() {
+    console.debug('call init() method');
+    console.debug('{ id:' + this.id + ', control:' + this.control +  '}');
+  }
+};
+
 var _mod = (function() {
+  var id = 'module'
   return {
-    info: function() {
-      return { id: 'amod', control: 'homepage' }
-    },
+    control: function() { return 'homepage'; },
+    id: function() { return id; },
     init: function() {
     }
   }
 }());
 
-coreJS.registerMod(_mod);
+coreJS.registerMod(Module);
